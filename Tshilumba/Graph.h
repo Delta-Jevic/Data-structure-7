86c486acbb9b321663cd4471939b06dbaf763a64
Delta-Jevic <113912460+Delta-Jevic@users.noreply.github.com>
2022-12-08 14:54:13 -0600

    Update

--- Tshilumba/Graph.h
+++ Tshilumba/Graph.h
@@ -1,21 +1,27 @@
-// General description of the ADT and supported operations – exportable operations only
-// Do not include any implementation details
+/*
+Name: Jevic Tshilumba
+Class: CSC 300
+Professor: Ken Gamradt
+Comment: This program creates a user-defined abstract Data type named Graph
+The Graph ADT will define and implement the following data types and operations.
+*/ 
+
 #pragma once
 typedef unsigned GElement;
 
 class Graph 
 {
-    public:                 
-
+    public:                                               // exportable
+                                                          // General description of each of the ADT operations/functions – exportable operations only
         explicit Graph(const std::string = "graph.dat");
         ~Graph();
         void dijkstra();
 
-    private:                
-
+    private:                                             // non-exportable
+                                                         // No private member documentation – implementation details are hidden/abstracted away
         enum GraphLimit {GRAPH_MAX = 25};
         Graph(const Graph &) = delete;
-        void fillAdjMatrix(const std::string );
+        void fillAdjMatrix(const std::string);
         void fillVisitedSet();
         unsigned getFirstNode() const;
         void view() const;
